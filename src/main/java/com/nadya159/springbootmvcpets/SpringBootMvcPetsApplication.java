aa9b826dc33fe7377fc1c935f6bceee781836134
Nadya159 <nadezdovna59@gmail.com>
2024-06-26 11:57:37 +0500

    Initial commit

---
+++ src/main/java/com/nadya159/springbootmvcpets/SpringBootMvcPetsApplication.java
@@ -0,0 +1,13 @@
+package com.nadya159.springbootmvcpets;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootMvcPetsApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringBootMvcPetsApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/nadya159/springbootmvcpets/SpringBootMvcPetsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.nadya159.springbootmvcpets;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringBootMvcPetsApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
